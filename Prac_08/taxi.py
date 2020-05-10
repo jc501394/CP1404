56f38fef682f0a2a6a759df80a99c2e83e977072
Costigan <aukacostigan@gmail.com>
2020-05-11 02:16:24 +1000

    Adding Prac_08 Files

---
+++ Prac_08/taxi.py
@@ -0,0 +1,11 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Test Taxi"""
+    my_taxi = Taxi("Silver Ferns 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
